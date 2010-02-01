70c131d97d154c17c7706e5c152308f73a277e6c
sarj <sarj1988@gmail.com>
2010-02-02 00:22:02 +0300

    HelloWorld class added
    asdfas
    df
    as
    df
    ad
    
    Signed-off-by: sarj <sarj1988@gmail.com>

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author S
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		
+	}
+
+}
