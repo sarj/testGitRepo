cfc660247b9da5c10871b73d480af24dcdc622af
sarj <sarj1988@gmail.com>
2010-02-02 00:26:33 +0300

    enhanced HelloWorld
    Signed-off-by: sarj <sarj1988@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello Git world");
 		
 		
 	}
